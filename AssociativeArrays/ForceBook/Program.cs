575bb10be01a366504b5664d9ce309039751f85b
Svetoslav Petrov <s.petrov.bg@gmail.com>
2020-11-26 12:37:42 +0200

    Finish Associative Arrays - Exercise

--- AssociativeArrays/ForceBook/Program.cs
+++ AssociativeArrays/ForceBook/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ForceBook
 {
@@ -6,7 +8,87 @@ namespace ForceBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SortedDictionary<string, List<string>> forceBook = new SortedDictionary<string, List<string>>();
+            string command = Console.ReadLine();
+            while (command != "Lumpawaroo")
+            {
+                string forceUser = "";
+                string forceSide = "";
+                string currCommand = "";
+                if (command.Contains("->"))
+                {
+                    forceUser = command.Split(" -> ")[0];
+                    forceSide = command.Split(" -> ")[1];
+                    currCommand = "->";
+                }
+                else
+                {
+                    forceSide = command.Split(" | ")[0];
+                    forceUser = command.Split(" | ")[1];
+                    currCommand = "|";
+                }
+                switch (currCommand)
+                {
+                    case "|":
+                        if (forceBook.ContainsKey(forceSide))
+                        {
+                            if (forceBook[forceSide].Contains(forceUser) == false)
+                            {
+                                forceBook[forceSide].Add(forceUser);
+                            }
+                        }
+                        else
+                        {
+                            bool isFound = false;
+                            foreach (var item in forceBook)
+                            {
+                                if (item.Value.Contains(forceUser))
+                                {
+                                    isFound = true;
+                                    break;
+                                }
+                            }
+                            if (isFound == false)
+                            {
+                                forceBook.Add(forceSide, new List<string>() { forceUser });
+                            }
+                        }
+                        break;
+                    case "->":
+                        foreach (var item in forceBook)
+                        {
+                            if (item.Value.Contains(forceUser))
+                            {
+                                string currForceSide = item.Key;
+                                forceBook[currForceSide].Remove(forceUser);
+                                break;
+                            }
+                        }
+
+                        if (forceBook.ContainsKey(forceSide))
+                        {
+                            forceBook[forceSide].Add(forceUser);
+                        }
+                        else
+                        {
+                            forceBook.Add(forceSide, new List<string>() { forceUser });
+                        }
+
+                        Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var item in forceBook.OrderByDescending(c => c.Value.Count).Where(c => c.Value.Count != 0))
+            {
+                Console.WriteLine($"Side: {item.Key}, Members: {item.Value.Count}");
+                foreach (var user in item.Value.OrderBy(n => n))
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, $"! {user}"));
+                }
+            }
         }
     }
 }
---
+++ AssociativeArrays/SoftUniExamResults/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUniExamResults
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Exam> exams = new List<Exam>();
+            SortedDictionary<string, Exam> examResult = new SortedDictionary<string, Exam>();
+            string command = Console.ReadLine();
+            while (command != "exam finished")
+            {
+                string userName = command.Split("-")[0];
+                string language = command.Split("-")[1];
+                if (language == "banned")
+                {
+                    if (examResult.ContainsKey(userName))
+                    {
+                        examResult.Remove(userName);
+                    }
+                    command = Console.ReadLine();
+                    continue;
+                }
+                int points = int.Parse(command.Split("-")[2]);
+
+                if (examResult.ContainsKey(userName))
+                {
+                    if (examResult[userName].Points < points)
+                    {
+                        examResult[userName].Points = points;
+                    }
+                    Exam exam = new Exam(language, points);
+                    exams.Add(exam);
+                }
+                else
+                {
+                    Exam exam = new Exam(language, points);
+                    exams.Add(exam);
+                    examResult.Add(userName, exam);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("Results:");
+            foreach (var item in examResult.OrderByDescending(p => p.Value.Points))
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, $"{item.Key} | {item.Value.Points}"));
+            }
+
+            SortedDictionary<string, int> allLangs = new SortedDictionary<string, int>();
+            foreach (var item in exams)
+            {
+                if (allLangs.ContainsKey(item.Language))
+                {
+                    allLangs[item.Language]++;
+                }
+                else
+                {
+                    allLangs.Add(item.Language, 1);
+                }
+            }
+
+            Console.WriteLine("Submissions:");
+            foreach (var item in allLangs.OrderByDescending(c => c.Value))
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, $"{item.Key} - {item.Value}"));
+            }
+        }
+    }
+
+    public class Exam
+    {
+        public string Language { get; set; }
+        public int Points { get; set; }
+
+        public Exam(string language, int points)
+        {
+            Language = language;
+            Points = points;
+        }
+    }
+}
