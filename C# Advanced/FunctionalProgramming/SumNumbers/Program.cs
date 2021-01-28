e90d30b962a690a7f2ce49bd97f36e78cd928b08
Svetoslav <s.petrov.bg@gmail.com>
2021-01-28 19:13:56 +0200

    First step in functional Programing

---
+++ C# Advanced/FunctionalProgramming/SumNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int[]> func = ReturnSumAndCountInArray;
+            Console.WriteLine(string.Join(Environment.NewLine, func(Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray()
+                )));
+
+            //Action<int[]> Printer = PrintSumAndCount;
+            //Printer(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+        }
+
+        private static void PrintSumAndCount(int[] obj)
+        {
+            Console.WriteLine(obj.Length);
+            Console.WriteLine(obj.Sum());
+        }
+
+        public static int[] ReturnSumAndCountInArray(int[] array)
+        {
+            int[] result = new int[2];
+            result[0] = array.Length;
+            result[1] = array.Sum();
+            return result;
+        }
+    }
+}
