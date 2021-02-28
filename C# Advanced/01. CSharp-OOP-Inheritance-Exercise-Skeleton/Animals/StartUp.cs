7090912ff7477a089ff29a8499343a4ec8968a80
Svetoslav <s.petrov.bg@gmail.com>
2021-02-28 18:43:46 +0200

    Finish homework Inheritance

--- C# Advanced/01. CSharp-OOP-Inheritance-Exercise-Skeleton/Animals/StartUp.cs
+++ C# Advanced/01. CSharp-OOP-Inheritance-Exercise-Skeleton/Animals/StartUp.cs
@@ -1,10 +1,69 @@
-﻿namespace Animals
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            List<Animal> animals = new List<Animal>();
+
+            string typeOfAnimal = Console.ReadLine();
+            while (typeOfAnimal != "Beast!")
+            {
+                string[] currAnimal = Console.ReadLine().Split();
+                string name = currAnimal[0];
+                int age = int.Parse(currAnimal[1]);
+                string gender = currAnimal[2];
+
+                if ((gender != "Male" && gender != "Female") || age < 0)
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+                else
+                {
+                    switch (typeOfAnimal)
+                    {
+                        case "Cat":
+                            if (name == "Tomcat")
+                            {
+                                Tomcat tomcat = new Tomcat(name, age, gender);
+                                animals.Add(tomcat);
+                            }
+                            else if (name == "Kitten")
+                            {
+                                Kitten kitten = new Kitten(name, age, gender);
+                                animals.Add(kitten);
+                            }
+                            else
+                            {
+                                Cat cat = new Cat(name, age, gender);
+                                animals.Add(cat);
+                            }
+                            break;
+                        case "Dog":
+                            Dog dog = new Dog(name, age, gender);
+                            animals.Add(dog);
+                            break;
+                        case "Frog":
+                            Frog frog = new Frog(name, age, gender);
+                            animals.Add(frog);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid input!");
+                            break;
+                    }
+                }
+                typeOfAnimal = Console.ReadLine();
+            }
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal.GetType().Name);
+                Console.WriteLine(animal);
+                animal.ProduceSound();
+            }
         }
     }
 }
---
+++ C# Advanced/01. CSharp-OOP-Inheritance-Exercise-Skeleton/Restaurant/Coffee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        //public Coffee(string name, double caffeine, decimal price = 3.5M, double milliliters = 50)
+        //    : base(name, price, milliliters)
+
+        private const double defaultMilliliters = 50;
+        private const decimal defaultPrice = 3.5M;
+        public Coffee(string name, double caffeine)
+            : base(name, defaultPrice, defaultMilliliters)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
