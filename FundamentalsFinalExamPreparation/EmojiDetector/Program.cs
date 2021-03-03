2df21f0093cee0f0db40ba67e7e3e8f044a1a536
Svetoslav <s.petrov.bg@gmail.com>
2021-03-03 20:24:44 +0200

    Fundamentals final exam preparation

---
+++ FundamentalsFinalExamPreparation/EmojiDetector/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EmojiDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([:*])\1(?<emoji>[A-Z][a-z]{2,})\1\1";
+            Regex regex = new Regex(pattern);
+
+
+            string text = Console.ReadLine();
+            long threshold =    text.ToCharArray()
+                                    .Where(c => char.IsDigit(c))
+                                    .Select(c => int.Parse(c.ToString()))
+                                    .Aggregate(1, (a, b) => a * b);
+
+
+
+            MatchCollection result = regex.Matches(text);
+
+            Console.WriteLine($"Cool threshold: {threshold}");
+            Console.WriteLine($"{result.Count} emojis found in the text. The cool ones are:");
+            foreach (Match item in result)
+            {
+                int sum = 0;
+                foreach (var symbol in item.Groups["emoji"].Value)
+                {
+                    sum += symbol;
+                }
+                if (sum > threshold)
+                {
+                    Console.WriteLine(item.Value);
+                }
+                
+            }
+
+             
+        }
+    }
+}
