4a9bbea062d99dcd4addf89dbba110e34fa25ab4
Svetoslav <s.petrov.bg@gmail.com>
2021-03-09 10:15:41 +0200

    first try asp.net

---
+++ C# Databases Advanced - EFCore/Linq/01. MusicHub Database_Skeleton/MusicHub/StartUp.cs
@@ -0,0 +1,96 @@
+﻿namespace MusicHub
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            MusicHubDbContext context = 
+                new MusicHubDbContext();
+
+            DbInitializer.ResetDatabase(context);
+
+            Console.WriteLine(ExportSongsAboveDuration(context, 4));
+        }
+
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            StringBuilder result = new StringBuilder();
+            var albums = context
+                        .Albums
+                        .Where(p => p.ProducerId == producerId)
+                        .Select(a => new
+                        {
+                            a.Name,
+                            a.ReleaseDate,
+                            ProducerName = a.Producer.Name,
+                            AlbumSongs = a.Songs.Select(s => new
+                            {
+                                SongName = s.Name,
+                                s.Price,
+                                SongWriterName = s.Writer.Name,
+                            })
+                        })
+                        .OrderByDescending(p => p.AlbumSongs.Sum(s => s.Price))
+                        .ToList();
+
+            foreach (var album in albums)
+            {
+                int count = 0;
+                result.AppendLine($"-AlbumName: {album.Name}");
+                result.AppendLine($"-ReleaseDate: {album.ReleaseDate.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture)}");
+                result.AppendLine($"-ProducerName: {album.ProducerName}");
+                result.AppendLine($"-Songs:");
+                foreach (var song in album.AlbumSongs.OrderByDescending(s => s.SongName).ThenBy(w => w.SongWriterName))
+                {
+                    result.AppendLine($"---#{++count}");
+                    result.AppendLine($"---SongName: {song.SongName}");
+                    result.AppendLine($"---Price: {song.Price:F2}");
+                    result.AppendLine($"---Writer: {song.SongWriterName}");
+                }
+                result.AppendLine($"-AlbumPrice: {album.AlbumSongs.Sum(s => s.Price):F2}");
+            }
+
+            return result.ToString().Trim();
+        }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            StringBuilder result = new StringBuilder();
+
+            var songs = context.Songs
+                        .Select(s => new
+                        {
+                            SongName = s.Name,
+                            Writer = s.Writer.Name,
+                            Performer = s.SongPerformers.Select(p => p.Performer.FirstName + " " + p.Performer.LastName).FirstOrDefault(),
+                            AlbumProducer = s.Album.Producer.Name,
+                            s.Duration,
+                        })
+                        .ToList()
+                        .Where(s => s.Duration.TotalSeconds > duration)
+                        .OrderBy(s => s.SongName)
+                        .ThenBy(s => s.Writer)
+                        .ThenBy(s => s.Performer);
+
+            int count = 0;
+            foreach (var song in songs)
+            {
+                result.AppendLine($"-Song #{++count}");
+                result.AppendLine($"---SongName: {song.SongName}");
+                result.AppendLine($"---Writer: {song.Writer}");
+                result.AppendLine($"---Performer: {song.Performer}");
+                result.AppendLine($"---AlbumProducer: {song.AlbumProducer}");
+                result.AppendLine($"---Duration: {song.Duration:c}");
+            }
+            
+            return result.ToString().Trim();
+        }
+    }
+}
---
+++ C# Databases Advanced - EFCore/Linq/AllAlbumsProducedByGivenProducer/StartUp.cs
@@ -0,0 +1,63 @@
+﻿using MusicHub.Data;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace AllAlbumsProducedByGivenProducer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            MusicHubDbContext musicHubDbContext = new MusicHubDbContext();
+
+            int producerId = int.Parse(Console.ReadLine());
+            Console.WriteLine(ExportAlbumsInfo(musicHubDbContext, producerId));
+        }
+
+        private static string ExportAlbumsInfo(MusicHubDbContext musicHubDbContext, int producerId)
+        {
+            StringBuilder result = new StringBuilder();
+            var albums = musicHubDbContext
+                        .Albums
+                        .Where(p => p.ProducerId == producerId)
+                        .Select(a => new
+                        {
+                            a.Name,
+                            a.ReleaseDate,
+                            ProducerName = a.Producer.Name,
+                            AlbumSongs = a.Songs.Select(s => new
+                            {
+                                SongName = s.Name,
+                                s.Price,
+                                SongWriterName = s.Writer.Name,
+                            })
+                            .OrderByDescending(s => s.SongName)
+                            .ThenBy(w => w.SongWriterName),
+                            AlbumPrice = a.Songs.Sum(s => s.Price),
+                        })
+                        .OrderByDescending(p => p.AlbumPrice)
+                        .ToList();
+
+            foreach (var album in albums)
+            {
+                int count = 0;
+                result.AppendLine($"-AlbumName: {album.Name}");
+                result.AppendLine($"-ReleaseDate: {album.ReleaseDate.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture)}");
+                result.AppendLine($"-ProducerName: {album.ProducerName}");
+                result.AppendLine($"-Songs:");
+                foreach (var song in album.AlbumSongs)
+                {
+                    result.AppendLine($"---#{++count}");
+                    result.AppendLine($"---SongName: {song.SongName}");
+                    result.AppendLine($"---Price: {song.Price:F2}");
+                    result.AppendLine($"---Writer: {song.SongWriterName}");
+                }
+                result.AppendLine($"-AlbumPrice: {album.AlbumPrice:F2}");
+            }
+
+            return result.ToString();
+        }
+    }
+}
